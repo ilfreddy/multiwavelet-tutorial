cbb6bf50de99167f39616e98cda20bc71d12e936
Stig Rune Jensen <stig.r.jensen@uit.no>
2021-10-15 15:01:23 +0200

    Hylleraas launch button

--- content/conf.py
+++ content/conf.py
@@ -88,7 +88,7 @@ html_theme_options = {
     "use_download_button": True,
     "home_page_in_toc": True,
     "launch_buttons": {
-        "binderhub_url": "https://mybinder.org",
+        "binderhub_url": "https://hylleraas.jupyterhub.uio.no/",
         "thebe": True,
         "notebook_interface": "jupyterlab",
     },
